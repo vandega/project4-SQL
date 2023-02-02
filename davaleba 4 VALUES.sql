b6c2582d4fd6bd81605570aba5784ca70b7c210a
Erko42071 <lukaerkom1@gmail.com>
2023-02-02 17:10:10 +0400

    Added Values

--- davaleba 4 VALUES.sql
+++ davaleba 4 VALUES.sql
@@ -1,36 +1,80 @@
 INSERT INTO Student (Student_id, Student_name, Lastname, Student_id_num, Email, Address, Birth_day, Sex)
-VALUES	(1, 'joni', 'sturua', 01001089305, 'jako.sturua@gmail.com', 'Tbilisi, gldani, VI m/r', '1997-08-13', 'male'),
-		(2, 'Jane', 'Doe', 654000, 'janedoe@example.com', '456 Oak Ave', '1998-02-14', 'Female'),
-		(3, 'Jim', 'Smith', 111111, 'jimsmith@example.com', '789 Pine Rd', '1997-03-21', 'Male'),
-		(4, 'Sarah', 'Johnson', 222222, 'sarahjohnson@example.com', '246 Maple St', '1996-04-01', 'Female'),
-		(5, 'Michael', 'Brown', 333333, 'michaelbrown@example.com', '135 Cedar Blvd', '1995-05-16', 'Male'),
-		(6, 'Emily', 'Davis', 444444, 'emilydavis@example.com', '768 Oak Rd', '1994-06-23', 'Female'),
-		(7, 'William', 'Miller', 555555, 'williammiller@example.com', '369 Maple Ave', '1993-07-04', 'Male'),
-		(8, 'Ashley', 'Wilson', 666666, 'ashleywilson@example.com', '159 Pine St', '1992-08-11', 'Female'),
-		(9, 'David', 'Anderson', 01253647, 'davidanderson@example.com', '753 Cedar Rd', '1991-09-17', 'Male'),
-		(10, 'Jessica', 'Thomas', 0100136587, 'jessicathomas@example.com', '246 Oak Blvd', '1990-10-25', 'Female');
+VALUES	(1, 'Joni', 'Sturua', 01001089305, 'jako.sturua@gmail.com', 'Tbilisi, Gldani, VI m/r', '1997-08-13', 'Male'),
+		(2, 'Luka', 'Erkomaishvili', 01008061456, 'lukaerkom1@gmail.com', ' Tbilisi, Vake, Ateni Street 18', '2001-08-05', 'Male'),
+		(3, 'Giorgi', 'Mirzoevi', 04001000849 ,'giorgimirzoevi5@yahoo.com', 'Tbilisi, Avlabari, Ketevan Tsamebulis 2', '1997-03-21', 'Male'),
+		(4, 'Natalia', 'Alisanashvili', 10001014117 , 'Natalia10@gmail.com', 'Tbilisi, Mitskevichi Street 3', '1996-04-01', 'Female'),
+		(5, 'Giorgi', 'Kikvadze', 01011035264 , 'kikva15@yahoo.com', 'Tbilisi, Varketili, II m/r', '1995-05-16', 'Male'),
+		(6, 'Liza', 'Berishvili', 18001023761, 'lizabero20@outlook.com', 'Tbilisi, Saburtalo Street 4', '1994-06-23', 'Female'),
+		(7, 'Mikheil', 'Rekhviashvili', 37001011730, 'mikhrekh@ocean.com', 'Tbilisi, Kazbegi Street 6', '1993-07-04', 'Male'),
+		(8, 'Saba', 'Somkhiashvili', 08001018718, 'sabasomkh50@gmail.com', 'Batumi, Abashidze Street 2 ', '1992-08-11', 'Male'),
+		(9, 'Giorgi', 'Giorgadze', 01008040201, 'giorgi2@.yahoo.com', 'Tbilisi, Vake , Paliashvili Street 20', '1991-09-17', 'Male'),
+		(10, 'Sophio', 'Gorgiladze', 01008062342, 'gorgiladzesophio1@example.com', 'Tbilisi, Sololaki , Amagleba Street 2', '1990-10-25', 'Female'),
+		(11,'Tinatin','Tinikashvili',01005020102,'tinatintinikashvili12@gmail.com','Tbilisi, Khiliani Street 35','2002-10-25','Female'),	
+		(12,'Luka','Vadachkoria',01002003456,'lukavadachkoria17@gmail.com','Tbilisi, Vake , Yifshidze Street 4','2001-04-28','Male'),	
+		(13,'Tariel','Vaxania',01002003004,'tarielvax2@yahoo.com','Kutaisi, Illa Chavchavadze Street 4','2005-10-02','Male'),
+		(14,'Sandro','Abutidze',01030003702,'sandroab@gmail.com','Kutaisi, Akaki Tserteli Street 4','1994-04-06','Male'),
+		(15,'Vaxo','Japaridze',01024055459,'japara25@yahoo.com','Tbilsi, Vake , Kavsadze Street 3','2006-02-10','Male'),
+		(16,'Sandro','Bakradze',01017013253,'sandrob@gmail.com','Tbilisi, Vera , Melikishvili Street 3','2001-05-10','Male'),
+		(17,'Elene','Wiklauri',49001004272,'eleneTs2@gmail.com','Batumi, Gogebashvili Street','1994-05-20','Female'),
+		(18,'Nuca','Pulariani',23001003389,'nutsap12@yahoo.com','Tbilisi, Gldani III m/r','1980-10-20','Female'),
+		(19,'Masho','Pertenava',35001084328,'mashoper15@gmail.com','Tbilisi, Vake , Abashidze Street 21','1999-11-20','Female'),
+		(20,'Gigi','Gvaramia',42001007426,'gigigv11@gmail.com','Tbilisi, Vake , Chavchavadze 21','2010-10-20','Male'),
+		(21,'Irakli','Khvadagadze',51001017797,'iraklikh@yahoo.com','Batumi, Giorgi Mazanishvili Street 10','2009-02-21','Male'),
+		(22,'Zuka','Managadze',35001045767,'zukaman@gmail.com','Kutaisi, Alexander Pushkin Street 21 ','1999-01-20','Male'),
+		(23,'Juba','Kaxidze',33001016406,'jubakh@mail.ru','Tbilisi, Vake, Abashidze Street 29','1974-03-10','Male'),
+		(24,'Giogi','Bregvadze',61001060467,'giorgibr04@gmail.com','Kutaisi, Giorgi Saakadze Street 20','2001-12-10','Male'),
+		(25,'Taso','Gvaramadze',33001045341,'tasogv15@gmail.com','Batumi,  Giorgi Mazniashvili Street 20 ','2000-07-20','Female'),
+		(26,'Vaso','Berikashviil',35001028665,'vasober@rambler.ru','Tbilisi, Shardeni Street 27 ','1998-02-10','Male'),
+		(27,'Dachi','Otiashvili',35001029453,'dachiot@outlook.com','Tbilisi, Vake  Ramishvili Street 2','1992-03-10','Male'),
+		(28,'Goga','Babunashvili',33001018945,'gogobab@gmail.com','Kutaisi, Anton Katalikosi Street 21 ','1990-09-09','Male'),
+		(29,'Tako','Gambashidze',61010001984,'takogamb@yahoo.com','Tbilisi, Digomi , Petritsis 4','1993-10-02','Female'),
+		(30,'Tamar','Baratashvili',01026000625,'tamarbar@gmail.com','Tbilisi, Mtatsminda , Amagleba Street 7 ','2001-02-02','Female'),
+		(31,'Alexandre','Bejuashvili',22001008151,'alexander15@gmail.com','Batumi, Kontsantine Gamsaxurdia Street 20','2002-03-03','Male'),
+		(32,'Aleko','Elisashvili',55001005840,'alekoelis@gmail.com','Tbilisi, Vera , Gogebashvili Street 20','2004-03-05','Male');
 
 
 INSERT INTO Lecture (Lecture_id, Lecture_name, Lastname, Lecture_id_num, Email, Address, Birth_day, Sex, Bank_account, Position)
-VALUES	(1, 'John', 'Doe', 12345678901, 'johndoe@example.com', '123 Main St', '1980-01-01', 'Male', '1234567890123456', 'Professor'),
-	  	(2, 'Jane', 'Doe', 23456789012, 'janedoe@example.com', '456 Oak Ave', '1981-02-14', 'Female', '234567890123456', 'math Professor'),
-	  	(3, 'Jim', 'Smith', 34567890123, 'jimsmith@example.com', '789 Pine Rd', '1982-03-21', 'Male', '34567890123456', 'english Professor'),
-   	   	(4, 'Sarah', 'Johnson', 45678901234, 'sarahjohnson@example.com', '246 Maple St', '1983-04-01', 'Female', '4567890123456', 'IT Professor'),
-	   	(5, 'Michael', 'Brown', 56789012345, 'michaelbrown@example.com', '135 Cedar Blvd', '1984-05-16', 'Male', '567890123456', 'history Professor');
-
+VALUES	(1, 'Venera', 'Chutkerashvili', 01005021534, 'venerachutkerashvili@uni.ge', 'Tbilisi , Vake , Ramishvili Street 3', '1980-01-01', 'Female', 'GE29TB0000000101904917', 'Full Professor'),
+	  	(2, 'Tamaz', 'Erkomaishvili', 01026000625, 'tamazerkomaishvili@uni.ge', 'Tbilisi , Vera , Gogebashvili Street 10', '1981-02-14', 'Male', 'GE29BG0000000102504917', 'Assistant Professor'),
+	  	(3, 'Lamara', 'Qurchishvili', 01017052656 , 'lamaraqurchishvili12@uni.ge', 'Tbilisi , Saburtalo Street 10', '1982-03-21', 'Female', 'GE29TB0000000123404917', 'Associate Professor'),
+   	   	(4, 'Zviad', 'Gagoshidze', 01024064764, 'zviadgagoshidze1@uni.ge', 'Tbilisi , Sololaki , Baratashvili Street 12', '1983-04-01', 'Male', 'GE29TB0000000101989017', 'Full Professor'),
+	   	(5, 'Erekle', 'Tsereteli', 62001041124, 'erekletsereteli@uni.ge', 'Tbilisi , Vake , Kekelidze Street 1', '1984-05-16', 'Male', 'GE29BG0000000909004917', 'Full Proffesor');
+		(6, 'Konstantine','Zandukeli',62004028147,'konstantinezandukeli@uni.ge','Tbilisi , Vake , Radiani Street 2','1970-12-01','Male','GE29TB0000000102224917','Assistant Professor'),
+		(7, 'Vasil','Oyroshidze',61004048743,'vasiloyroshidze@uni.ge','Tbilisi , Saburtalo , Dolidze Street 21','1980-10-20','Male','GE29BG0000000101907657','Associate Professor'),
+		(8, 'Maia','Gabunia',01011091273,'maiagabunia@uni.ge','Tbilisi , Naxalovla , Marjanishvili Street 15','1991-05-02','Female','GE29TB0000000101004917','Associate Professor'),
 
 INSERT INTO Subject (Subject_id, Subject_name)
-VALUES (1, 'Math'),
+VALUES (1, 'Mathematics'),
        (2, 'Science'),
        (3, 'History'),
        (4, 'English'),
-       (5, 'Geography');
+       (5, 'Geography'),
+	   (6, 'Georgian'),
+	   (7, 'German'),
+	   (8, 'Informatics'),
+	   (9, 'Religion'),
+	   (10, 'Sports'),
+	   (11, 'Chemisty'),
+	   (12, 'Methodology'),
+	   (13, 'Art'),
+	   (14, 'Biology'),
+	   (15, 'Music'),
 
 
 INSERT INTO Faculty(Faculty_id, Faculty_name)
 VALUES	(1, 'Science'),
 		(2, 'Arts'),
-		(3, 'Commerce');
+		(3, 'Commerce'),
+		(4,	'Economics'),
+		(5,	'Phsycology'),
+		(6,	'Statistics'),
+		(7,	'Journalism'),
+		(8,	'Buisness Managment'),
+		(9, 'Law'),
+		(10, 'Medical'),
+		(11, 'International relations'),
+		(12, 'Agronomy');
+
 
 
 INSERT INTO Subject_timing(Time_id, Subject_name, Subject_time)
@@ -42,7 +86,23 @@ VALUES 	(1, 1, '10:00:00'),
 		(6, 3, '13:45:00'),
 		(7, 4, '11:30:00'),
 		(8, 4, '16:00:00'),
-		(9, 5, '12:00:00');
+		(9, 5, '12:00:00'),
+		(10,5,'12:30:00'),
+		(11,6,'20:30:00'),
+		(12,6,'12:45:00'),
+		(13,7,'10:30:00'),
+		(14,7,'20:00:00'),
+		(15,8,'15:30:00'),
+		(16,8,'09:30:00'),
+		(17,9,'11:30:00'),
+		(18,9,'20:00:00'),
+		(19,10,'09:30:00'),
+		(20,10,'10:30:00'),
+		(21,11,'17:00:00'),
+		(22,11,'18:30:00'),
+		(23,12,'12:30:00'),
+		(24,12,'13:30:00');
+
 
 
 INSERT INTO On_faculty(On_faculty_id, Faculty_id, Subject_id)
