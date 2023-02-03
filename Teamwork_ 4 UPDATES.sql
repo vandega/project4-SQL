6c492ef3b0edac0f3255ad7f245d0f6d6c983d17
Erko42071 <lukaerkom1@gmail.com>
2023-02-03 20:48:12 +0400

    UPDATES update

--- Teamwork_ 4 UPDATES.sql
+++ Teamwork_ 4 UPDATES.sql
@@ -2,13 +2,50 @@
 
 UPDATE student SET
 Student_name = 'Salome',
-Lastname = 'asatiani',
+Lastname = 'Asatiani',
 Student_id_num = 01001011123,
 Email = 'salomeasatiani@gmail.com',
 WHERE Student_id = 1;
--- +2
 
+UPDATE student SET 
+Student_name = 'Gocha',
+Lastname = 'Chertkoevi',
+WHERE Student_id = 15; 
+
+UPDATE student SET 
+Lastname = 'Gogava',
+WHERE Student_id = 13;
+
+UPDATE student SET
+Email = 'Nutsapulariani200@gmail.com',
+WHERE Student_id = 19;
+
+UPDATE student SET 
+Birth_day = '2000-05-25',
+Student_name = "Anastasia",
+Sex = 'Female',
+WHERE Student_id = 32;
+
+UPDATE student SET 
+Student_id_num = '01005045678',
+WHERE Student_id = 6;
+
+UPDATE student SET
+Address = 'Zestafoni , Saguramos Street 3',
+WHERE Student_id = 23;
+---------------------
 
 UPDATE Lecturer SET
+Lastname = 'Erkomaishvili',
+Birth_day = "1960-12-10",
+WHERE Lecture_id = 3;
 
--- +2
+UPDATE Lecturer SET
+Lastname = 'Sturua',
+WHERE Lecture_id = 7;
+
+UPDATE Lecture SET 
+Position = 'Full Proffesor',
+WHERE Lecture_id = 8;
+
+----------------------
